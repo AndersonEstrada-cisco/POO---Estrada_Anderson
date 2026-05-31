c4ffd11520db7dab5309020e237fee100a886d18
AndersonEstrada-cisco <a.estradawml5@gmail.com>
2026-05-31 16:59:23 -0500

    Delete ReservaCanchaDeportiva directory

--- ReservaCanchaDeportiva/CrudReserva.java
+++
@@ -1,8 +0,0 @@
-public interface CrudReserva {
-
-    void registrarReserva(String cedula, int horas);
-    void actualizarReserva(String cedula, int nuevasHoras);
-    void eliminarReserva(String cedula);
-    int calcularTotalHoras();
-    String clienteMayorReserva();
-}
